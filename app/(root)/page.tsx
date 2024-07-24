41d56e7d940a2cedcddc05f70c90cead188b1184
Matteo Anselmi <matteo.anselmi@saef.it>
2024-07-24 16:43:16 +0200

    implement auth, appwrite

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getLoggedInUser } from '@/lib/actions/user.action';
 import React from 'react'
 
-const Home = () => {
-  const loggedIn = { firstName: 'Matteo', lastName: 'Anselmi', email: 'matteo.anselmi@saef.it' };
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
   return (
     <section className="home">
       <div className='home-content'>
@@ -12,14 +13,14 @@ const Home = () => {
           <HeaderBox 
           type="greeting"
           title="Benvenuto"
-          user={loggedIn?.firstName || 'Guest'}
+          user={loggedIn?.name || 'Guest'}
           subtext="Accedi e gestisci la tua azienda e le tue persone"
           />
 
           <TotalBalanceBox 
           accounts={[]}
           totalBanks={1}
-          totalCurrentBalance={20.35}
+          totalCurrentBalance={12020.35}
           />
         </header>
 
